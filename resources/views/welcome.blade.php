56a8ac3a10d6915c4b562d816e0c3e34bbf2ef7d
liufang <ylf1991scau@gmail.com>
2016-07-24 23:45:34 +0000

    first commit on laravel application

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">First Laravel application!</div>
             </div>
         </div>
     </body>
