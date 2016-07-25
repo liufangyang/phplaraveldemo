82f0e2705b1910b1295ff16aeb074c0f6aa855b2
liufang <ylf1991scau@gmail.com>
2016-07-25 00:59:38 +0000

    modify main css file.Add home page and navigation header and delete default welcome page

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+    <nav>
+        <ul>
+       <li><a herf="#">Home</a></li>
+       <li><a herf="#">About</a></li>
+       <li><a herf="#">Blog</a></li>
+       <li><a herf="#">Contact</a></li>
+       </ul>
+    </nav>
+</header>
